727f2e911097275469968cbc5b19dec96066a93f
MOsama12345 <mohammad.osama@koderlabs.com>
2024-02-19 17:59:50 +0500

    back to sqaure one

--- src/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Button(props) {
-const Counter = () =>{props.clickF(props.click+1)}
-return (
-    <button onClick={Counter}>{props.click} Clicks</button>
-  )
-}
